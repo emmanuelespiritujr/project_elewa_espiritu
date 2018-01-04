d5668eb3fd40f01fc93112ff30c16972a311f8c7
emmanuelespiritujr <espiriem@sheridanc.on.ca>
2018-01-03 23:59:28 -0500

    Added Splash Screen which shows logo of app

---
+++ app/src/main/java/com/example/jespiritu/project_elewa_espiritu/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.jespiritu.project_elewa_espiritu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
