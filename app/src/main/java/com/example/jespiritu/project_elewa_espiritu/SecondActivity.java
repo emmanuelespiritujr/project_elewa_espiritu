542c3386dd7ed75b39963cb55562b2c302d56a1c
salimelewa <elewas@sheridancollege.ca>
2018-01-04 09:13:58 -0500

    Added the arrival and departure times, and the constant time interval variable.

--- app/src/main/java/com/example/jespiritu/project_elewa_espiritu/SecondActivity.java
+++ app/src/main/java/com/example/jespiritu/project_elewa_espiritu/SecondActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 
 public class SecondActivity extends Activity {
 
+    private static int CLARKSON_DEP_TIME_INTERVAL = 6;
+    private static int PORTCREDIT_DEP_TIME_INTERVAL = 15;
+
+    private static int CLARKSON_ARR_TIME_INTERVAL = 6;
+    private static int OAKVILLE_ARR_TIME_INTERVAL = 14;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
